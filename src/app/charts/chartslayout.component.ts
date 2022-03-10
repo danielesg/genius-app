210c452cb213f5a8047eab97dfc6eec40c801cf2
danielesg <die0001@mix.wvu.edu>
2022-03-10 04:53:17 -0500

    Added charts layout component

---
+++ src/app/charts/chartslayout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'charts',
+    templateUrl: 'chartslayout.component.html',
+    styleUrls: ['chartslayout.component.css']
+})
+export class ChartsLayoutComponent {
+
+}
